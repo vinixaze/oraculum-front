57ba73b5c766471a10248326c720dfea22547592
Caio-J-Pontes <cj.pontes22@gmail.com>
2025-10-21 21:11:11 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../assets/OraculumLogo.png'; // ajuste o caminho conforme sua estrutura
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
         <div className="header-logo">
-          <span className="logo-text">ORACULUM</span>
+          <img src={logo} alt="Logo Oraculum" className="logo-img" />
         </div>
         <a href="#" className="header-link">
           PORTAL DO GESTOR
